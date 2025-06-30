807ef3c5c318fd6b23de98ace8ed99aba8c8b67a
Kaiming2001 <xukm2024@lzu.edu.cn>
2025-06-30 17:08:24 +0800

    Update GO similarity calculation.R

--- Fig2b.GO相似性计算.R
+++ Fig2b.GO相似性计算.R
@@ -1,22 +1,22 @@
-##获取GOid之间的相似性
+##Obtain the similarity between GO ids
 BiocManager::install("simplifyEnrichment")
 BiocManager::install("org.Hs.eg.db")
 library(simplifyEnrichment)
 library(readxl)
-df <- read_excel("D:/盐生草/结果/tree/基因家族扩张和收缩/GO/GOID.xlsx")
+df <- read_excel("D:/hara/result/tree/genefamily/GO/GOID.xlsx")
 GOIDs <- as.character(df$GOID)
 head(GOIDs)
 mat = GO_similarity(GOIDs)
 df1=simplifyGO(mat,method = "kmeans")
-dist_mat <- as.dist(1 - mat)  # 相似性转为距离
-hc <- hclust(dist_mat, method = "average")  # 层次聚类
+dist_mat <- as.dist(1 - mat)  
+hc <- hclust(dist_mat, method = "average")  
 groups <- cutree(hc, k = 5)
-#转换为cytoscape输入数据
+#Convert to CytoScape input data
 net = reshape2::melt(mat)
-net = net[which(net$value > 0.3),]  #删掉一些相似性特别低的点
-net = net[which(as.character(net$Var1)>as.character(net$Var2)),]   #删除回路，比如有A到B线和B到A的线，只保留A到B的
+net = net[which(net$value > 0.3),] 
+net = net[which(as.character(net$Var1)>as.character(net$Var2)),] 
 head(net)
 head(df1)
-setwd("D:/盐生草/结果/盐爪爪盐节木/GO/")
+setwd("D:/hara/result/tree/genefamily/GO/")
 write.table(net,file = "net.txt",col.names = T,row.names = F,quote = F,sep = "\t")
 write.table(df1,file = "df.txt",col.names = T,row.names = F,quote = F,sep = "\t")
